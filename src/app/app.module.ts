4a40b70a13f6eeaf7b61d49c0723fec776f9cc24
Lucas Braga <mobileolucas@hotmail.com>
2024-10-17 18:05:12 -0300

    estilizaçao: trocando modal da busca personalizada home, adicionando dropdown de busca

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -53,6 +53,7 @@ import {MatOptionModule} from '@angular/material/core';
 import {NgImageFullscreenViewModule} from 'ng-image-fullscreen-view';
 import {NgcCookieConsentModule, NgcCookieConsentConfig} from 'ngx-cookieconsent';
 import { PoliticaComponent } from './politica/politica.component';
+import { CustomDropdownComponent } from './core/components/custom-dropdown/custom-dropdown.component';
 
 registerLocaleData(localePt);
 
@@ -127,7 +128,8 @@ const cookieConfig: NgcCookieConsentConfig = {
     LancamentosComponent,
     NextToastComponent,
     CustomSearchComponent,
-    PoliticaComponent
+    PoliticaComponent,
+    CustomDropdownComponent
   ],
   imports: [
     CommonModule,
